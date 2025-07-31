d00225c2ecd0bbea1c8d0203d33c74d3e0f1b13c
ArjunTheDev <128279719+ArjunTheDev@users.noreply.github.com>
2025-07-31 20:08:14 +0530

    Added env file
    Added env file

--- react/node_express/appMiddleware.js
+++ react/node_express/appMiddleware.js
@@ -1,5 +1,7 @@
 const jwt = require('jsonwebtoken');
-const SECRET_KEY = 'your_secret_key';
+require('dotenv').config();
+
+const SECRET_KEY = process.env.SECRET_KEY;
 
 function authenticateUser(req, res, next) {
   const token = req.headers.authorization;
